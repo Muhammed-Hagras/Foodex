6b13d0393cce9e8f8bf00e0aece69b2e52403d43
Muhammed-Hagras <40708938+Muhammed-Hagras@users.noreply.github.com>
2023-06-20 23:46:04 +0200

    layout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Logo from "../assets/Logo.png";
+import { AiFillFacebook, AiFillGithub, AiFillInstagram } from "react-icons/ai";
+import Image from "next/image";
+import css from "../styles/Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={css.container}>
+      <span>ALL RIGHT RESERVED</span>
+      <div className={css.social}>
+        <AiFillFacebook size={45} />
+        <AiFillGithub size={45} />
+        <AiFillInstagram size={45} />
+      </div>
+      <div className={css.logo}>
+        <Image src={Logo} alt="" width={50} height={50} />
+        <span>Foodex</span>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import css from "../styles/Header.module.css";
+import Logo from "../assets/Logo.png";
+import Image from "next/image";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={css.header}>
+      {/* Left Side  */}
+      <div className={css.logo}>
+        <Image src={Logo} alt="" width={50} height={50}></Image>
+      </div>
+      {/* menu side  */}
+      <ul className={css.menu}>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Contact</li>
+      </ul>
+      {/* right side  */}
+      <div className={css.rightSide}>
+        <div className={css.cart}>
+          <AiOutlineShoppingCart size={35} color="#2e2e2e" />
+          <div className={css.cartAmount}>1</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
